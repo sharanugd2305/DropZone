b1cc70787b73ffd45397b967a5697bd1a1aab933
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-04-03 19:13:56 +0530

    Add server.js with Express setup and middleware configuration

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookiesparser from 'cookie-parser';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(cookiesparser());
+
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port http://localhost:${process.env.PORT}`);
+});
