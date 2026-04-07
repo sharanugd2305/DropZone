04a3370b17a376fe1b7a071e1e3445e3b2c5abc0
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-04-07 20:28:45 +0530

    Refactor main.jsx to integrate ClerkProvider and BrowserRouter for authentication and routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { ClerkProvider } from "@clerk/react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+createRoot(document.getElementById("root")).render(
+  <ClerkProvider publishableKey={clerkPubKey}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ClerkProvider>,
+);
