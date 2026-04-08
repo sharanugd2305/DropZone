7b8f681851a4ef77ce36004f3109e6db616f4ad8
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-04-08 20:35:19 +0530

    Add ProtectedRoute component for handling authentication redirects

---
+++ client/src/components/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+import {
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+} from "@clerk/clerk-react";
+
+export default function ProtectedRoute({ children }) {
+  return (
+    <>
+      <SignedIn>{children}</SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+    </>
+  );
+}
