cadf959ecea4111da73410aebf05e1db8c94f53d
sharanugd2305 <sharanudodamani0077@gmail.com>
2026-04-15 17:21:50 +0530

    delete controller

---
+++ server/controllers/delete.controllers.js
@@ -0,0 +1,12 @@
+import fileModels from "../models.js/file.models";
+export const deleteFileByID = async (req,res) =>{
+  try {
+    const deletedfile = await File.findByIdAndDelete(req.params.id);
+    if(!deletedfile){
+      return res.status(404).json({message: "file not found"});
+    }
+    res.status(200).json({message: "file deleted successfully"});
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+}
