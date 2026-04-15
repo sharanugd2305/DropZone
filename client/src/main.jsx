8daa78b09c5374d7c99de0f6a26582368503258a
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-04-15 19:51:00 +0530

    Wrap App component with UserContext provider for context management

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,17 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
+import UserContext from "./context/UserContext.jsx";
 
 const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 createRoot(document.getElementById("root")).render(
   <ClerkProvider publishableKey={clerkPubKey}>
     <BrowserRouter>
-      <App />
+      <UserContext>
+        <App />
+      </UserContext>
     </BrowserRouter>
   </ClerkProvider>,
 );
+
