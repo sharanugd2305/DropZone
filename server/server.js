0e8110fe4705b9c87c30e776602b45b6257512a5
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-04-15 19:52:31 +0530

    Refactor server.js to improve middleware setup and code formatting

--- server/server.js
+++ server/server.js
@@ -1,21 +1,31 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import cookiesparser from 'cookie-parser';
-import connectDB from './config/db.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookiesparser from "cookie-parser";
+import connectDB from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import UserRouter from "./routes/user.js";
 
 dotenv.config();
 
 connectDB(); // Connect to MongoDB
 const app = express();
-app.use(cors());
+app.use(clerkMiddleware());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(cookiesparser());
 
-app.get('/', (req, res) => {
-    res.send('Server is running!');
+app.get("/", (req, res) => {
+  res.send("Server is running!");
 });
 
+app.use("/api/users", UserRouter);
+
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port http://localhost:${process.env.PORT}`);
+  console.log(`Server is running on port http://localhost:${process.env.PORT}`);
 });
