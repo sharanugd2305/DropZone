d693d9e01be92d02e822c5174077d13ce2c6f68a
sharanugd2305 <sharanudodamani0077@gmail.com>
2026-04-17 16:53:46 +0530

    Fix ownerId type in file schema and ensure required fields are set

--- server/models/file.models.js
+++ server/models/file.models.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 const fileSchema = new mongoose.Schema({
     dropzonefile: { type: String, required: true },
-    ownerId: { type: mongoose.Schema.Types.ObjectId, ref: "User", },
+    ownerId: { type: String, ref: "User", required: true },
+    parentId: { type: mongoose.Schema.Types.ObjectId, ref: "Folder", default: null },
+    name: { type: String, required: true },
+    size: { type: String },
     createdAt: { type: Date, default: Date.now },
-
 });
 export default mongoose.model("File", fileSchema); 
