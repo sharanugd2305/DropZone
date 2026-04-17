fc5c01c469fa68b7b37e3db4335a489d6e891dc2
sharanugd2305 <sharanudodamani0077@gmail.com>
2026-04-17 16:54:00 +0530

    Add authentication middleware and refactor file upload route

--- server/routes/file.routes.js
+++ server/routes/file.routes.js
@@ -1,7 +1,15 @@
 import express from "express";
 import { uploadfile } from "../controllers/upload.controllers.js";
 import { deleteFileByID } from "../controllers/delete.controllers.js";
+import { deleteFolder } from "../controllers/folder.controllers.js";
+import upload from "../middlewares/multer.js";
+import protect from "../middlewares/auth.js";
+
 const fileRouter = express.Router();
-fileRouter.post("/upload", uploadfile);
+
+fileRouter.use(protect); // Apply auth middleware to all file routes
+fileRouter.post("/upload", upload.single("file"), uploadfile);
 fileRouter.delete("/delete/:id", deleteFileByID);
+fileRouter.delete("/folder/:id", deleteFolder);
+
 export default fileRouter;
