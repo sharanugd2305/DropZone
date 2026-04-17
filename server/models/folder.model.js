61457f9a06bc5510b551de3416414ffaae9ed8fd
sharanugd2305 <sharanudodamani0077@gmail.com>
2026-04-17 16:58:45 +0530

    Add folder model with schema definition for folder management

---
+++ server/models/folder.model.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const folderSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    ownerId: { type: String, ref: "User", required: true },
+    parentId: { type: mongoose.Schema.Types.ObjectId, ref: "Folder", default: null },
+    createdAt: { type: Date, default: Date.now },
+});
+
+export default mongoose.model("Folder", folderSchema);
