0c247f124d8a9f47fcb166fe8ea1a052e0b0f084
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-04-19 15:52:58 +0530

    Add starred field to folder schema for managing starred status

--- server/models/folder.model.js
+++ server/models/folder.model.js
@@ -4,6 +4,7 @@ const folderSchema = new mongoose.Schema({
     name: { type: String, required: true },
     ownerId: { type: String, ref: "User", required: true },
     parentId: { type: mongoose.Schema.Types.ObjectId, ref: "Folder", default: null },
+    starred: { type: Boolean, default: false },
     createdAt: { type: Date, default: Date.now },
 });
 
