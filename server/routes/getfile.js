2040fb28bf889c63d6feb359861ff011531f12f4
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-04-19 15:54:03 +0530

    Add route for sharing files by ID

--- server/routes/getfile.js
+++ server/routes/getfile.js
@@ -4,6 +4,8 @@ import protect from "../middlewares/auth.js";
 
 const getfileRouter = express.Router();
 
+getfileRouter.get("/share/:id", getfileControllers.getSharedItemById);
+
 getfileRouter.use(protect); // Apply auth middleware to all get routes
 getfileRouter.get("/files", getfileControllers.getAllFiles);
 getfileRouter.get("/files/:id", getfileControllers.getFileByID);
