f2b55970d2631fdbb26a15c699893956d9511e0e
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-04-25 11:03:45 +0530

    Update toggleStarredItem to use 'returnDocument: "after"' for consistency in response

--- server/controllers/star.controllers.js
+++ server/controllers/star.controllers.js
@@ -18,7 +18,7 @@ export const toggleStarredItem = async (req, res) => {
     const updatedItem = await Model.findOneAndUpdate(
       { _id: id, ownerId: req.userId },
       { starred },
-      { new: true }
+      { returnDocument: "after" }
     );
 
     if (!updatedItem) {
